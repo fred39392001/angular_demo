910d9ca02033042969415dec7e6112a27cbd44bf
fred39392001 <fred39392001@gmail.com>
2021-10-01 23:04:56 +0800

    Update the app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule
